974e1125b6c17ac40a1158ab944bf702df9d24a2
glebson1988 <glebson1988@yandex.ru>
2026-01-12 08:18:24 +0200

    8 bytes

--- main.go
+++ main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	fileName := "messages.txt"
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	buffer := make([]byte, 8)
+
+	for {
+		n, err := file.Read(buffer)
+
+		if n > 0 {
+			fmt.Printf("read: %s\n", string(buffer[:n]))
+		}
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
