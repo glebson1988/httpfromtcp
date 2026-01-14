96ba8d07af5c5c86341e8b077cd1fb2b02c96f6e
glebson1988 <glebson1988@yandex.ru>
2026-01-14 15:08:54 +0200

    Request && request tests

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,62 @@
 package request
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read from reader: %w", err)
+	}
+	reqStr := string(data)
+	reqLineEnd := strings.Index(reqStr, "\r\n")
+	if reqLineEnd == -1 {
+		return nil, fmt.Errorf("invalid request: missing CRLF")
+	}
+	reqLineStr := reqStr[:reqLineEnd]
+	reqLine, err := parseRequestLine(reqLineStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse request line: %w", err)
+	}
+	return &Request{
+		RequestLine: *reqLine,
+	}, nil
+}
+
+func parseRequestLine(line string) (*RequestLine, error) {
+	parts := strings.Split(line, " ")
+	if len(parts) != 3 {
+		return nil, fmt.Errorf("invalid request line: %s", line)
+	}
+	method, target, version := parts[0], parts[1], parts[2]
+	if method == "" {
+		return nil, fmt.Errorf("invalid HTTP method: %s", method)
+	}
+	for _, r := range method {
+		if r < 'A' || r > 'Z' {
+			return nil, fmt.Errorf("invalid HTTP method: %s", method)
+		}
+	}
+	if version != "HTTP/1.1" {
+		return nil, fmt.Errorf("invalid HTTP version: %s", version)
+	}
+	httpVersion := strings.TrimPrefix(version, "HTTP/")
+	return &RequestLine{
+		Method:        method,
+		RequestTarget: target,
+		HttpVersion:   httpVersion,
+	}, nil
+}
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,11 +1,72 @@
 package request
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestNewRequest(t *testing.T) {
-	assert.Equal(t, "TheTestagen", "TheTestagen")
+func TestGoodRequestLine(t *testing.T) {
+	r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+}
+
+func TestGoodRequestLineWithPath(t *testing.T) {
+	r, err := RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+}
+
+func TestGoodPOSTRequestWithPath(t *testing.T) {
+	r, err := RequestFromReader(strings.NewReader("POST /brew HTTP/1.1\r\nHost: localhost\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "POST", r.RequestLine.Method)
+	assert.Equal(t, "/brew", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+}
+
+func TestInvalidNumberOfPartsInRequestLine(t *testing.T) {
+	_, err := RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost\r\n\r\n"))
+	require.Error(t, err)
+}
+
+func TestInvalidMethodRequestLine(t *testing.T) {
+	_, err := RequestFromReader(strings.NewReader("get / HTTP/1.1\r\nHost: localhost\r\n\r\n"))
+	require.Error(t, err)
+	_, err = RequestFromReader(strings.NewReader("123 / HTTP/1.1\r\nHost: localhost\r\n\r\n"))
+	require.Error(t, err)
+}
+
+func TestInvalidVersionInRequestLine(t *testing.T) {
+	_, err := RequestFromReader(strings.NewReader("GET / HTTP/2.0\r\nHost: localhost\r\n\r\n"))
+	require.Error(t, err)
+	_, err = RequestFromReader(strings.NewReader("GET / HTTP/1.0\r\nHost: localhost\r\n\r\n"))
+	require.Error(t, err)
+	_, err = RequestFromReader(strings.NewReader("GET / HTTTP/1.1\r\nHost: localhost\r\n\r\n"))
+	require.Error(t, err)
+}
+
+func TestMissingRequestLine(t *testing.T) {
+	_, err := RequestFromReader(strings.NewReader("\r\nHost: localhost\r\n\r\n"))
+	require.Error(t, err)
+}
+
+func TestExtraSpacesInRequestLine(t *testing.T) {
+	_, err := RequestFromReader(strings.NewReader("GET  /  HTTP/1.1\r\nHost: localhost\r\n\r\n"))
+	require.Error(t, err)
+}
+
+func TestEmptyRequest(t *testing.T) {
+	_, err := RequestFromReader(strings.NewReader(""))
+	require.Error(t, err)
 }
